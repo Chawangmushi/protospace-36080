c2edf01b2cb42bdbdca73b8cf20a314fb066dcc2
Chawangmushi <satoshi0112@gmail.com>
2021-07-12 16:40:50 +0900

    1st commint

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    @name = user.name
+    @profile = user.profile
+    @occupation = user.occupation
+    @position = user.position
+    @prototypes_user = user.prototypes
+
+  end
+
+
+end
